29651450869b738e1a2365d0900c5e84dd9a1306
Luis Bautista <alias__sk8@hotmail.com>
2024-07-11 16:42:19 -0500

    update email

--- helpers/email.js
+++ helpers/email.js
@@ -1,35 +1,45 @@
 import nodemailer from "nodemailer";
 
-//Hacer tambein emailRegistro para Admin
 export const emailRegistro = async (datos) => {
     const { email, id_servicio, token } = datos;
 
     const transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
+        host: "smtp.zoho.com",
+        port: 465,
+        secure: true,
         auth: {
-            user: "7c7829a033233d",
-            pass: "489d4c64c20a60"
+            user: "prueba@prueba.com",
+            pass: "prueba123"
         }
     });
+
+    // const transport = nodemailer.createTransport({
+    //     host: "sandbox.smtp.mailtrap.io",
+    //     port: 2525,
+    //     auth: {
+    //         user: "7c7829a033233d",
+    //         pass: "489d4c64c20a60"
+    //     }
+    // });
 
 
     //Informacion del email
 
     const info = await transport.sendMail({
-        from: '"Meganet Portal - Administrador - Portal" <meganetperu@meganet.com>',
+        from: '"TalentIQ - Portal" <prueba@prueba.com>',
         to: email,
-        subject: "Meganet - Comprueba tu Cuenta",
-        text: "Comprueba tu cuenta en Meganet Portal",
-        html: ` <p>Hola: ${email} Comprueba tu cuenta en Meganet</p>
+        subject: "TalentIQ - Comprueba tu Cuenta",
+        text: "Comprueba tu cuenta en TalentIQ",
+        html: ` <p>Hola: ${email} Comprueba tu cuenta en TalentIQ</p>
         <p>Tu cuenta ya esta casi lista solo debes comprobarla en el siguiente enlace: </p>
-        <a href="${process.env.FRONTEND_URL}/miportalmeganet/confirmar/${token}">Comprobar Cuenta</a>
+        <a href="${process.env.FRONTEND_URL}/login/confirmar/${token}">Comprobar Cuenta</a>
         <p>Si tu no creaste esta cuenta, puedes ingnorar este mensaje</p>
         
         
         `,
     })
 };
+
 
 export const emailOlvidePasswordCliente = async (datos) => {
     const { email, id_servicio, token } = datos;
@@ -47,13 +57,13 @@ export const emailOlvidePasswordCliente = async (datos) => {
     //Informacion del email
 
     const info = await transport.sendMail({
-        from: '"Meganet Portal - Cliente - Portal" <meganetperu@meganet.com>',
+        from: '"TalentIQ Portal - Cliente - Portal" <prueba@prueba.com>',
         to: email,
-        subject: "Meganet - Reestablece tu Password",
-        text: "Reestablece tu Password para entrar al Meganet Portal",
+        subject: "TalentIQ - Reestablece tu Password",
+        text: "Reestablece tu Password para entrar al TalentIQ Portal",
         html: ` <p>Hola: ${email} has solicitado reestablecer tu password</p>
         <p>Sigue el siguiente enlace para generar un nuevo password: </p>
-        <a href="${process.env.FRONTEND_URL}/miportalmeganet/olvide-password/${token}">Reestablecer Password</a>
+        <a href="${process.env.FRONTEND_URL}/miportalTalentIQ/olvide-password/${token}">Reestablecer Password</a>
         <p>Si tu no solicitaste este cambio , puedes ingnorar este mensaje</p>
         
         
@@ -77,9 +87,9 @@ export const emailOlvidePasswordAdmin = async (datos) => {
     //Informacion del email
 
     const info = await transport.sendMail({
-        from: '"Meganet Portal - Administrador - Portal" <meganetperu@meganet.com>',
+        from: '"TalentIQ Portal - Administrador - Portal" <TalentIQperu@TalentIQ.com>',
         to: email,
-        subject: "Meganet - Reestablece tu Password",
+        subject: "TalentIQ - Reestablece tu Password",
         text: "Reestablece tu Password para entrar al Administrador",
         html: ` <p>Hola: ${nombres} has solicitado reestablecer tu password</p>
         <p>Sigue el siguiente enlace para generar un nuevo password: </p>
